6cf5ea2a76092ee02d14521822cba0c5077c9d9e
Roman T <rt.hagbo@gmail.com>
2022-04-12 04:16:48 +0500

    Дз №12 - «TypeScript» - Список покупок (задача со звёздочкой)

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -2,46 +2,7 @@ import Cart from '../service/Cart';
 import Book from '../domain/Book';
 import MusicAlbum from '../domain/MusicAlbum';
 import Movie from '../domain/Movie';
-
-const bookObj = {
-  id: 1001,
-  name: 'War and Piece',
-  author: 'Leo Tolstoy',
-  price: 2000,
-  pages: 1225
-};
-
-const musicAlbumObj = {
-  id: 1008,
-  name: 'Meteora',
-  author: 'Linkin Park',
-  price: 900
-};
-const movieObj = {
-  id: 1022,
-  name: 'Мстители',
-  originalName: 'The Avengers',
-  year: 2012,
-  country: 'США',
-  taglines: '«Avengers Assemble!»',
-  genres: 'фантастика, боевик, фэнтези, приключения',
-  time: 137,
-  price: 3030,
-};
-
-const book = new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225);
-const musicAlbum = new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900);
-const movie = new Movie(
-  1022, 
-  'Мстители',
-  'The Avengers',
-  2012,
-  'США',
-  '«Avengers Assemble!»',
-  'фантастика, боевик, фэнтези, приключения',
-  137,
-  3030,
-);
+import Gadget from '../domain/Gadget';
 
 test('new card should be empty', () => {
   const cart = new Cart();
@@ -49,29 +10,66 @@ test('new card should be empty', () => {
   expect(cart.items.length).toBe(0);
 });
 
-test('cart add Book and MusicAlbum', () => {
+test('cart add product', () => {
   const cart = new Cart();
-  cart.add(book);
-  cart.add(musicAlbum);
-  cart.add(movie);
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Movie(1022, 'Мстители', 'The Avengers', 2012, 'США', '«Avengers Assemble!»', 'фантастика, боевик, фэнтези, приключения', 137, 3030));
+  cart.add(new Gadget(2000, 'Huawei', 'p40 pro', 100000));
 
-  expect(cart.items).toEqual([bookObj, musicAlbumObj, movieObj]);
+  const bookObj: Book = { id: 1001, name: 'War and Piece', author: 'Leo Tolstoy', price: 2000, pages: 1225 };
+  const musicAlbumObj: MusicAlbum = { id: 1008, name: 'Meteora', author: 'Linkin Park', price: 900 };
+  const huaweiObj: Gadget = { id: 2000, name: 'Huawei', model: 'p40 pro', price: 100000, count: 1 };
+  const movieObj: Movie = {
+    id: 1022,
+    name: 'Мстители',
+    originalName: 'The Avengers',
+    year: 2012,
+    country: 'США',
+    taglines: '«Avengers Assemble!»',
+    genres: 'фантастика, боевик, фэнтези, приключения',
+    time: 137,
+    price: 3030,
+  };
+
+  expect(cart.items).toEqual([bookObj, musicAlbumObj, movieObj, huaweiObj]);
+});
+
+test('cart add product count', () => {
+  const cart = new Cart();
+  cart.add(new Gadget(2000, 'Huawei', 'p40 pro', 100000));
+  cart.add(new Gadget(2000, 'Huawei', 'p40 pro', 100000, 1));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+
+  const huaweiObj: Gadget = { id: 2000, name: 'Huawei', model: 'p40 pro', price: 100000, count: 2 };
+  const musicAlbumObj: MusicAlbum  = { id: 1008, name: 'Meteora', author: 'Linkin Park', price: 900 };
+
+  expect(cart.items).toEqual([huaweiObj, musicAlbumObj]);
 });
 
 test('cart sum', () => {
   const cartOne = new Cart();
   const cartTwo = new Cart();
-  cartTwo.add(book);
-  cartTwo.add(musicAlbum);
+  cartTwo.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cartTwo.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
 
   expect(cartOne.sum()).toEqual(0);
   expect(cartTwo.sum()).toEqual(2900);
 });
 
+test('cart sum Gadget', () => {
+  const cart = new Cart();
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Gadget(2000, 'Huawei', 'p40 pro', 100000, 2));
+
+  expect(cart.sum()).toEqual(200900);
+});
+
 test('cart sumDiscount', () => {
   const cart = new Cart();
-  cart.add(book);
-  cart.add(musicAlbum);
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
 
   expect(cart.sumDiscount(10)).toEqual(2610);
 });
@@ -85,17 +83,45 @@ test('cart sumDiscount error', () => {
 
 test('cart delete', () => {
   const cart = new Cart();
-  cart.add(book);
-  cart.add(musicAlbum);
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
   cart.del(1008);
+
+  const bookObj: Book = {
+    id: 1001,
+    name: 'War and Piece',
+    author: 'Leo Tolstoy',
+    price: 2000,
+    pages: 1225
+  };
 
   expect(cart.items).toEqual([bookObj]);
 });
 
 test('cart delete error', () => {
   const cart = new Cart();
-  cart.add(book);
-  cart.add(musicAlbum);
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
 
   expect(() => cart.del(1011)).toThrow(`Товар c id = 1011 не найден`);
+});
+
+test('cart countPlus', () => {
+  const cart = new Cart();
+  cart.add(new Gadget(2000, 'Huawei', 'p40 pro', 100000));
+  cart.countPlus(2000);
+  cart.countPlus(2000, 2);
+  cart.countPlus(2000, 0);
+  cart.countPlus(3333);
+
+  expect(cart.items).toEqual([new Gadget(2000, 'Huawei', 'p40 pro', 100000, 4)]);
+});
+
+test('cart countMinus', () => {
+  const cart = new Cart();
+  cart.add(new Gadget(2000, 'Huawei', 'p40 pro', 100000, 2));
+  cart.countMinus(2000);
+  cart.countMinus(3333);
+
+  expect(cart.items).toEqual([new Gadget(2000, 'Huawei', 'p40 pro', 100000, 1)]);
 });
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -3,31 +3,35 @@ import Buyable from '../domain/Buyable';
 export default class Cart {
   private _items: Buyable[] = [];
 
+  // метод добавляет товар в карзину
   add(item: Buyable): void {
-    this._items.push(item);
+    const index: number = this.searchId(item.id);
+
+    if (index === -1) { this._items.push(item) };
+    if (index != -1) { this.countPlus(item.id, item.count) };
   }
 
+  // метод для вывода товаров в карзине
   get items(): Buyable[] {
     return [...this._items];
   }
 
-  // Функция, считающая суммарную стоимость (без учёта скидки)
+  // метод, считающий суммарную стоимость (без учёта скидки)
   sum(): number {
     if (this._items.length > 0) {
-      return this.items.reduce((a, b) => a + b.price, 0);
+      return this.items.reduce((a, b) => {
+        if (b.count) { return a + b.count * b.price };
+        return a + b.price
+      }, 0);
     }
     return 0;
   }
 
-  // Функция, считающая суммарную стоимость (с учётом скидки) 
+  // метод, считающий суммарную стоимость (с учётом скидки) 
   sumDiscount(discount: number): number {
     try {
-      if (discount < 0) {
-        throw new Error('Скидка не может быть меньше 0%');
-      }
-      if (discount > 100) {
-        throw new Error('Скидка не может быть больше 100%');
-      }
+      if (discount < 0) { throw new Error('Скидка не может быть меньше 0%') };
+      if (discount > 100) { throw new Error('Скидка не может быть больше 100%')};
 
       const sum: number = this.sum();
       return sum - sum / 100 * discount;
@@ -36,18 +40,41 @@ export default class Cart {
     }
   }
 
-  // Функция, позволяющая удалять уже добавленный в корзину объект по полю id
+  // метод, позволяющий удалять уже добавленный в корзину объект по полю id
   del(id: number): void {
     try {
-      const index = this._items.findIndex((value: { id: number; }) => value.id === id);
+      const index: number = this.searchId(id);
 
-      if (index === -1) {
-        throw new Error(`Товар c id = ${id} не найден`);
-      }
+      if (index === -1) { throw new Error(`Товар c id = ${id} не найден`) };
 
       this._items.splice(index,1);
     } catch (err) {
       throw err;
+    }
+  }
+
+  // метод находит индекс товара по его ID
+  searchId(id: number): number {
+    return this._items.findIndex((value: { id: number; }) => value.id === id);
+  }
+
+  // метод увеличивает количество девайсов 
+  countPlus(id: number, itemCount: number = 1): void {
+    const index: number = this.searchId(id);
+    let count: number | undefined = this._items[index]?.count;
+
+    if (count) {
+      this._items[index].count = count + itemCount;
+    }
+  }
+
+  // метод уменьшает количество девайсов 
+  countMinus(id: number): void {
+    const index: number = this.searchId(id);
+    let count: number | undefined = this._items[index]?.count;
+
+    if (count && count > 1) {
+      this._items[index].count = count - 1;
     }
   }
 }
